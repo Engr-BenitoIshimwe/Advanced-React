79b45a987f67f8080724cdb6469392b5333f9df0
Benito Ishimwe <benishimwe31@gmail.com>
2022-05-15 01:25:15 +0200

    Advanced-React

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className='container'>
+      <h2>Scandiweb Developer Ishimwe Benito</h2>
+    </div>
+  );
+}
+
+export default App;
