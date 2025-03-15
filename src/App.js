1280e1c60216669d57ea925220f48145d46e6633
ISHIMWEBenito <benishimwe31@gmail.com>
2025-03-15 02:58:14 +0100

    Modified the file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 function App() {
   return (
     <div className='container'>
-      <h2>Benito Developer Employed as a Developer</h2>
+      <h2>Benito Developer Employed Paid as a Developer</h2>
     </div>
   );
 }
